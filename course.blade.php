4bb2aa0ce2fd6c1695218c6f526c1557906c2e13
Ashish8787 <69979718+Ashish8787@users.noreply.github.com>
2020-10-13 18:53:39 +0530

    Add files via upload

---
+++ course.blade.php
@@ -0,0 +1,66 @@
+@extends('generalmaster')
+@section('content')
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="col-sm-3" style="text-align: center;color:maroon;font-family: 'Times New Roman'">
+                <h2>AFTER 10TH COURSES</h2>
+            </div>
+            <div class="col-sm-6" style="text-align: center;color:maroon;font-family: 'Times New Roman'">
+                <h2>AFTER 12TH COURSES</h2>
+            </div>
+            <div class="col-sm-3" style="text-align: center;color:maroon;font-family: 'Times New Roman'">
+                <h2>AFTER GRADUATION</h2>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+             <div class="col-lg-3" style="text-align: center">
+
+                <ul>
+                    <h3>POLYTECHNIC BRANCH</h3>
+                    <p>CIVIL ENGINEERING</p>
+                    <p>MECHANICAL ENGINEERING</p>
+                    <p>ELECTRICAL ENGINEERING</p>
+                </ul>
+             </div>
+                 <div class="col-sm-3" style="text-align: center">
+                    <h3>B.TECH COURSES</h3>
+                    <ul>
+
+                            <P>AGRICULTURE ENGINEERING</P>
+                            <P>BIOTECH ENGINEERING</P>
+                            <P>CIVIL ENGINEERING</P>
+                            <P>COMPUTER SCIENCE & ENGINEERING</P>
+                            <P>ELECTRICAL ENGINEERING</P>
+                            <P>MECHANICAL ENGINEERING</P>
+                    </ul>
+                </div>
+                  <div class="col-sm-3" style="text-align: center">
+                      <h3>OTHER COURSES</h3>
+                      <ul>
+                          <p>B.B.A</p>
+                          <p>B.C.A</p>
+                          <p>B.Pharma</p>
+                          <p>D.Pharma</p>
+                          <p>Polytechnic lateral Entry (ME-PROD)</p>
+                          <p>Polytechnic lateral Entry (Electrical Engg.)</p>
+                          <p>Polytechnic lateral Entry (Civil Engg.)</p>
+                      </ul>
+                  </div>
+
+                  <div class="col-sm-3" style="text-align: center">
+                      <h3>MBA COURSES</h3>
+                      <p>M.B.A (HR)</p>
+                      <p>M.B.A (I.B)</p>
+                      <p>M.B.A (IT)</p>
+                      <p>M.B.A (MARKETING)</p>
+                      <p>M.B.A (FINANCE)</p>
+
+                  </div>
+        </div>
+    </div>
+
+
+@endsection()
